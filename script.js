2f18414eb720b3db2f5a19e6f873e666186f03ac
Dioconde <97199160+Liberojr@users.noreply.github.com>
2023-02-03 19:29:45 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+function fechar(){
+    var modal = document.querySelector('.GeneralModal2')
+    modal.style.display = 'none';
+}
+
+function acao(){
+    var modal = document.querySelector('.GeneralModal2')
+    modal.style.display = 'block';
+}
+function fechar2(){
+    var modal = document.querySelector('.GeneralModal3')
+    modal.style.display = 'none';
+}
+
+function acao2(){
+    var modal = document.querySelector('.GeneralModal3')
+    modal.style.display = 'block';
+}
+function fechar3(){
+    var modal = document.querySelector('.GeneralModal4')
+    modal.style.display = 'none';
+}
+
+function acao3(){
+    var modal = document.querySelector('.GeneralModal4')
+    modal.style.display = 'block';
+}
+function fechar4(){
+    var modal = document.querySelector('.GeneralModal5')
+    modal.style.display = 'none';
+}
+
+function acao4(){
+    var modal = document.querySelector('.GeneralModal5')
+    modal.style.display = 'block';
+}
+function fechar5(){
+    var modal = document.querySelector('.HomeModal')
+    modal.style.display = 'none';
+}
+
+function acao5(){
+    var modal = document.querySelector('.HomeModal')
+    modal.style.display = 'block';
+}
